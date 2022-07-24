875c01a6e592ee0b0c72acc6dd2e068c7fa2919f
Lander-Hatsune <wangwwx@yeah.net>
2022-07-24 15:12:20 +0800

    importlib; use width to filter '/'

--- recog.py
+++ recog.py
@@ -1,34 +1,42 @@
-import numpy as np
-import matplotlib.pyplot as plt
-from PIL import Image
-from model import Net
 import torch
+import numpy as np
+from . import model
+from PIL import Image
+import matplotlib.pyplot as plt
+import importlib.resources as pkg_resources
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-model = Net().to(device)
-model.load_state_dict(torch.load('Net.pt'))
+net = model.Net().to(device)
+with pkg_resources.path(__package__, 'Net.pt') as path:
+    net.load_state_dict(torch.load(path))
 
 def ocr1digit(digit:np.ndarray):
-    assert(digit.shape == (14, 9))
-    feed = torch.tensor(digit / 256, dtype=torch.float).unsqueeze(0).to(device)
-    out = model(feed).max(1)[1]
-    return str(out.item()) if out != 10 else '-'
+    feed = torch.tensor(digit / 255, dtype=torch.float).unsqueeze(0).to(device)
+    out = net(feed).max(1)[1]
+    return str(out.item()) if out != 10 else '*'
 
 def getkda(frame:np.ndarray):
     # shape: [height, width, nchannels]
-    # kda: [7:21, 1667:1743]
+    # kda: [7:21, 1666:1743]
     res = ''
-    img = Image.fromarray(frame[7:21, 1667:1743]).convert('L')
+    img = Image.fromarray(frame[7:21, 1666:1743]).convert('L')
     imgarr = np.array(img)
-    
+
     is_text = False
+    text_start = -1
     for icol in range(imgarr.shape[1]):
-        if sum(np.sort(imgarr[:, icol])[-3:]) > 300:
+        if sum(np.sort(imgarr[:, icol])[-3:]) > 250:
             is_text = True
+            if text_start == -1:
+                text_start = icol
         else:
             if is_text:
                 is_text = False
-                res += ocr1digit(imgarr[:, icol - 9:icol])
-
+                if icol - text_start >= 6: # minimum width of '1'
+                    digit = imgarr[:, max(0, icol - 9):icol]
+                    res += ocr1digit(digit)
+                else:
+                    res += '-'
+                text_start = -1
     return res
--- train.py
+++ train.py
@@ -13,14 +13,14 @@ dataloader = DataLoader(KDADataset(), batch_size=64, shuffle=True)
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-model = Net().to(device)
+net = Net().to(device)
 loss_fn = nn.CrossEntropyLoss()
 optimizer = optim.Adam(model.parameters())
 
 for epoch in range(EPOCH):
     for batchcnt, (imgs, labels) in enumerate(dataloader):
         imgs, labels = imgs.to(device), labels.to(device)
-        preds = model(imgs)
+        preds = net(imgs)
         loss = loss_fn(preds, labels)
 
         optimizer.zero_grad()
@@ -29,4 +29,4 @@ for epoch in range(EPOCH):
 
         print(f'loss: {loss:.7f}')
 
-torch.save(model.state_dict(), 'Net.pt')
+torch.save(net.state_dict(), 'Net.pt')
