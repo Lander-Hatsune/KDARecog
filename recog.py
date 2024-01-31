5fb5e18e96cf3c566bd1dce320fe3198e3aec5a6
Lander-Hatsune <wangwwx@yeah.net>
2024-01-31 10:59:59 +0800

    fix dataset

--- recog.py
+++ recog.py
@@ -19,7 +19,7 @@ def ocr1digit(digit: np.ndarray):
     # if True: #out.item() in [4, 7, 8, 9, 11]:
     #     (Image.fromarray(digit)
     #      .convert('L')
-    #      .save(f'data-predict/{out.item()}-{np.random.randint(0, 0xFFFF):04X}.png'))
+    #      .save(f'data-predict/{out.item()}/{np.random.randint(0, 0xFFFF):04X}.png'))
     if out == 10:
         return '*'
     elif out == 11:
@@ -58,10 +58,11 @@ def getdigits(imgarr):
 def getkda(frame: np.ndarray):
     # shape: [height, width, nchannels]
     # kda: [7:21, 1663:1743] +[7:21, 1653:1743]+
-    img = Image.fromarray(frame[7:21, 1656:1743]).convert('L')
+    img = Image.fromarray(frame[7:21, 1658:1743]).convert('L')
     imgarr = np.array(img)
     res = getdigits(imgarr)
 
+    # print(res)
     if not res or not re.match(r'\d+-\d+-\d+', res):
         return None
     else:
@@ -75,6 +76,7 @@ def getgametime(frame: np.ndarray):
     imgarr = np.array(img)
     res = getdigits(imgarr)
 
+    # print(res)
     if not res or not re.match(r'\d\d[\*-:]?\d\d', res):
         return None
     else:
