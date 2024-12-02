f59b1b6a4bfff9af66c82bb2221f3d912865a8d5
Lander-Hatsune <wangwwx@yeah.net>
2024-12-02 21:42:26 +0800

    add scoreboard recog; use autocontrast

--- recog.py
+++ recog.py
@@ -2,7 +2,7 @@ import re
 import torch
 import numpy as np
 from . import model
-from PIL import Image
+from PIL import Image, ImageOps
 import matplotlib.pyplot as plt
 import importlib.resources as pkg_resources
 
@@ -75,22 +75,51 @@ def getkda(frame: np.ndarray):
     # shape: [height, width, nchannels]
     # kda: [7:21, -254:-160]
     slice = cutBlack(frame)
-    img = Image.fromarray(slice[7:21, -254:-160]).convert('L')
+    img = ImageOps.autocontrast(
+        Image.fromarray(slice[7:21, -254:-160]).convert('L'), cutoff=10)
     imgarr = np.array(img)
     res = getdigits(imgarr)
+
+    scoreboard = _getscoreboard(slice)
 
     # print(res)
     if not res or not re.match(r'\d+-\d+-\d+', res):
         return None
     else:
-        return tuple(map(int, res.split('-')))
+        return tuple(map(int, res.split('-'))), scoreboard
+
+def getscoreboard(frame: np.ndarray):
+    slice = cutBlack(frame)
+    return _getscoreboard(slice)
+
+
+def _getscoreboard(slice: np.array):
+    img0 = Image.fromarray(slice[7:21, -362:-339]).convert('L')
+    img1 = Image.fromarray(slice[7:21, -324:-301]).convert('L')
+
+    # red is dimmer than blue
+    if np.sort(np.reshape(img0, (-1,)))[-3:].sum() > \
+        np.sort(np.reshape(img1, (-1,)))[-3:].sum():
+        ally_to_enemy = True
+    else:
+        ally_to_enemy = False
+
+    img0_ac = ImageOps.autocontrast(img0, cutoff=10)
+    img1_ac = ImageOps.autocontrast(img1, cutoff=10)
+    res0 = getdigits(np.array(img0_ac))
+    res1 = getdigits(np.array(img1_ac))
+    print((res0, res1) if ally_to_enemy else (res1, res0))
+    if not res0.isdigit() or not res1.isdigit():
+        return None
+    return (res0, res1) if ally_to_enemy else (res1, res0)
 
 
 def getgametime(frame: np.ndarray):
     # shape: [height, width, nchannels]
     # gametime: [7:21, -64:]
     slice = cutBlack(frame)
-    img = Image.fromarray(slice[7:21, -64:]).convert("L")
+    img = ImageOps.autocontrast(
+        Image.fromarray(slice[7:21, -64:]).convert("L"), cutoff=10)
     imgarr = np.array(img)
     res = getdigits(imgarr)
     # img.save(f"KDARecog/data/{res.replace(':', 'c').replace('*', 'x')}-{np.random.randint(0, 0xFFFF):04X}.png")
